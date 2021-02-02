b242ab5d2fa05de066b09d3fea851138c8917121
dachanisfull@gmail.com <dachan.kim>
2021-02-03 00:31:17 +0900

    링크 보드 레이아웃

--- src/components/linkBoard.vue
+++ src/components/linkBoard.vue
@@ -1,14 +1,46 @@
 <template>
-  <div>
-    링크보드
+  <div id="link-board">
+    <div class="wrapper">
+      <div class="wrapper__link">링크</div>
+      <div class="wrapper__preview">프리뷰</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'linkBoard'
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../common.scss";
+
+.wrapper {
+  margin: 0 auto;
+  padding: 0 10px;
+  width: 100%;
+  max-width: 1295px;
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+}
+
+.wrapper {
+  &__link {
+    padding: 0 20px;
+    max-width: 768px;
+    width: 100%;
+    flex: 1 0 375px;
+    background-color: red;
+    box-sizing: border-box;
+  }
+  &__preview {
+    max-width: 385px;
+    width: 100%;
+    flex: 1 1 385px;
+    background-color: blue;
+  }
+}
 
 </style>
