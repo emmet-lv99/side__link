976d0bff18cc4bf9e631573f70d86e8363f3fd32
dachanisfull@gmail.com <dachan.kim>
2021-02-04 01:08:22 +0900

    링크 영역, 아이템 추가 버튼, 웰컴 메시지 추가

--- src/components/linkBoard.vue
+++ src/components/linkBoard.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="link-board">
     <div class="wrapper">
-      <div class="wrapper__link">링크</div>
+      <div class="wrapper__link">
+        <div class="link__content">
+          <div class="link__url">
+            <span class="link__url--path">https://linklinklink.vip/username</span>
+            <span class="link__url--share">공유</span>
+          </div>
+          <div class="link__link-area">
+            <b-button class="link-area__btn--add" type="is-primary" expanded>+ 새로운 링크 추가</b-button>
+            <link-board-welcome v-if="noItemMsg"/>
+          </div>
+        </div>
+      </div>
       <div class="wrapper__preview">프리뷰</div>
     </div>
   </div>
 </template>
 
 <script>
+import linkBoardWelcome from '@/components/linkBoardWelcome.vue'
+
 export default {
-  name: 'linkBoard'
+  name: 'linkBoard',
+  components: {
+    linkBoardWelcome
+  },
+  data: () => ({
+    noItemMsg: true
+  })
 }
 </script>
 
@@ -30,8 +49,9 @@ export default {
   &__link {
     padding: 0 20px;
     max-width: 768px;
+    display: flex;
+    justify-content: center;
     flex: 1 0 400px;
-    background-color: red;
     box-sizing: border-box;
   }
   &__preview {
@@ -40,6 +60,41 @@ export default {
     flex: 1 0 320px;
     background-color: blue;
     box-sizing: border-box;
+  }
+}
+
+.link__content {
+  padding-top: 113px;
+  width: 600px;
+  .link {
+    &__url {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 30px;
+      width: 100%;
+      height: 60px;
+      border-radius: 8px;
+      background: $--color-white;
+      &--path {
+        color: $--color-wetgray-80;
+      }
+      &--share {
+        display: inline-block;
+        height: 40px;
+        line-height: 40px;
+        color: $--color-primary;
+        cursor: pointer;
+      }
+    }
+    &__link-area {
+      padding-top: 20px;
+      button.link-area__btn--add {
+        margin-bottom: 0px;
+        border-radius: 8px !important;
+        height: 60px !important;
+      }
+    }
   }
 }
 
---
+++ src/components/linkBoardWelcome.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="link-board-welcome">
+    <img src="@/assets/welcome.png" alt="" class="welcome__img">
+    <p class="welcome__msg">
+      <span class="welcome__msg--deco">환영합니다!</span>
+      새로운 링크를 만들고,<br/>
+      사람들과 나의 멋진 일상을<br/>
+      공유할 수 있어요!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'linkBoardWelcome'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../common.scss";
+
+#link-board-welcome {
+  padding-top: 80px;
+  text-align: center;
+}
+
+[class*="msg"] {
+  font: {
+    size: 32px;
+    weight: $--font-bold;
+  }
+  color: $--color-wetgray-60;
+}
+
+.welcome {
+  &__img {
+    line-height: 0;
+  }
+  &__msg {
+    line-height: 1.44em;
+    &--deco {
+      display: block;
+      font-size: 24px;
+      color: $--color-primary;
+    }
+  }
+}
+</style>
